26a6b4cab67cfc93c45055ffe34bdc63713dcc1e
DD-DIVYA <divyadhakshin66@gmail.com>
2024-09-22 10:48:18 +0530

    Add files via upload

---
+++ concatenation.py
@@ -0,0 +1,40 @@
+a="helloworldinPython"
+print('' in a)
+print(a.capitalize())
+print(a.center(25,'*'))
+print(a.count("in"))
+print(a.endswith ("non"))
+print(a.find("y"))
+print(a.isalpha())
+print(a.isalnum())
+print(a.isdigit())
+print('-'.join(a))
+print(a.ljust(45,'#'))
+print(a.rjust(45,'#'))
+print(a.isupper())
+print(a.islower())
+print(a.isspace())
+print(a.lstrip('he'))
+print(a.rstrip('thon'))
+print(a.maketrans('e','f'))
+print(a.translate(a))
+print (max(a))
+print (min(a))
+print(a.replace('y','r'))
+print(a.rfind('y'))
+print(a.rindex('o',6))
+print(a.split())
+print(a.startswith ("h"))
+print(a.strip())
+print(a.swapcase())
+print(a.title())
+text = "program is fun"
+print(text.zfill(15))
+
+
+
+
+
+
+
+
